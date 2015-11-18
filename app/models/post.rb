bf56068fd8131e22b229e5c22adc8c793e576564
Sam Corin <samcorin@gmail.com>
2015-11-18 21:43:29 +0900

    Add comments to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
+	has_many :comments
 	has_attached_file :image, styles: { medium: "500x300#", small: "350x250#" }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
